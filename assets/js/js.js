dc7df0b1fb04cce134e7d58858b81dad9a27ab6b
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-04 20:35:16 -0700

    added the function to display date to the html

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,5 +1,22 @@
 
 
+$(document).ready(function () {
 
+    //this displays the time 
+    var currentDay = $("#currentDay");
+    currentDay.text(moment().format("dddd, MMMM Do"));
 
-var currentDate = $("#currentDay").datepicker("getDate");
+    // Current Time
+    // Here we want to use setInterval to constantly update the time
+    let updateTime = function () {
+        let currentTime = moment().format('h:mm:ss')
+        //change this to set a var and use that to update past, present and future
+        $("#time").text(currentTime)
+    }
+
+    // To initally set the times, we will call the functions
+    updateTime();
+
+    // To continuously call the functions, we will use setInterval
+    setInterval(updateTime, 1000);
+})
